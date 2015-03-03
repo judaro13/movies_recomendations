fcf643baeed18f046abcbae312dd6084bcee367b
Juliana Davila <judaro13@gmail.com>
2015-03-03 15:36:30 -0500

    minnor fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, except: [:index]
+  before_action :set_user, :only => [:show, :edit, :update, :destroy, :vote]
 
   # GET /users
   # GET /users.json
